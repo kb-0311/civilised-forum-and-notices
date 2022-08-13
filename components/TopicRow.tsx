e693ab07907fb91da9d8dbd4feb27873d1f6b10d
Kanishka <kanishka.bansode@gmail.com>
2022-08-13 05:41:52 +0530

    local small change

--- components/TopicRow.tsx
+++ components/TopicRow.tsx
@@ -15,11 +15,10 @@ function TopicRow({ index, topic }: Props) {
   return (
     <div className="flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b">
       <p>{index + 1}</p>
-      <ChevronUpIcon className="h-4 w-4 flex-shrink-0 text-green-400" />
       <Avatar seed={String(topic.toLowerCase())} />
-      <p className="flex-1 truncate">r/{topic}</p>
+      <p className="flex-1 truncate">{topic}</p>
       <Link href={`/topic/${String(topic)}`}>
-        <div className="cursor-pointer rounded-full bg-blue-500 px-3 text-white">
+        <div className="cursor-pointer rounded-full bg-orange-500 px-3 text-white">
           View
         </div>
       </Link>
